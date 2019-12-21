773cb90e3a1bae454aed3ca1bb3be327cca14df9
JunHyeock Park <wnsgur6311@gmail.com>
2019-12-21 15:41:21 +0900

    webpack pwa manifest settings

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin"); // src index.html -> public index.html
+const WebpackPwaManifest = require("webpack-pwa-manifest");
 
 module.exports = {
   entry: "./src/index.js",
@@ -21,6 +22,20 @@ module.exports = {
   plugins: [
     new HtmlWebpackPlugin({
       template: "./src/index.html"
+    }),
+    new WebpackPwaManifest({
+      name: "리액트 메모",
+      short_name: "메모",
+      description: "리액트로 제작한 메모앱입니다.",
+      background_color: "#ffffff",
+      crossorigin: "use-credentials",
+      theme_color: "#eeeeee",
+      icons: [
+        {
+          src: path.resolve("src/assets/Icon.png"),
+          sizes: [96, 128, 192, 256, 384, 512]
+        }
+      ]
     })
   ]
 };
